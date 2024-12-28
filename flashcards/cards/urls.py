2a96c7253f765dfb8878388a5cb4bef3d9653d22
hebamesluh1 <hebamesluh@gmail.com>
2024-12-28 13:03:37 +0200

    add apps urls

---
+++ flashcards/cards/urls.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+from django.urls import path
+
+urlpatterns = [
+    path("",TemplateView.as_view(template_name="cards/base.html"),name="home"),
+]
+
+
+#Note :  we can used as_view when the view file not have any logic but view a templates , so django give us as_view to do that without any effort . 
