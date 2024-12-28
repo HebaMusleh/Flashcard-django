085edbb55ec741aa6d601e053614592324888d7c
hebamesluh1 <hebamesluh@gmail.com>
2024-12-28 13:20:21 +0200

    add card and card list pages

--- flashcards/cards/urls.py
+++ flashcards/cards/urls.py
@@ -1,9 +1,12 @@
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 from django.urls import path
 
-urlpatterns = [
-    path("",TemplateView.as_view(template_name="cards/base.html"),name="home"),
-]
-
-
+# urlpatterns = [
+#     path("",TemplateView.as_view(template_name="cards/base.html"),name="home"),
+# ]
 #Note :  we can used as_view when the view file not have any logic but view a templates , so django give us as_view to do that without any effort . 
+
+from . import views
+urlpatterns = [
+path("",views.CardListView.as_view(),name="card-list"),
+]
